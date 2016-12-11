616dc63ab31ba99f07dea5683f8c95d591a15b8f
Ola Klingberg <ola@olaklingberg.com>
2016-12-11 11:55:56 -0500

    Adds newMessage and createMessage events.

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Dummy sender',
+        text: 'So far, this is a message created on the server, i.e. not sent by anyone else',
+        createdAt: 123456
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
